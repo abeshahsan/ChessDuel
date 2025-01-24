3b81221ce6ac5321e8e3c5cc9948ae3407d7f10e
abeshahsan <abeshahsan2002@gmail.com>
2025-01-25 01:24:28 +0600

    refactor: simplify MdNavMenu component by removing unused state and handlers

--- src/app/components/navbar/md_nav_menu.tsx
+++ src/app/components/navbar/md_nav_menu.tsx
@@ -7,27 +7,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSidebarOpen } from "@/app/store/client_socket_slice";
 import { RootState } from "@/app/store";
 
-export default function MdNavMenu({ pages }: { pages: string[] }) {
-	const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-	const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+export default function MdNavMenu({ }: { pages: string[] }) {
 
 	const dispatch = useDispatch();
 	const sidebarOpen = useSelector((state: RootState) => state.clientSocket.sidebarOpen);
 
-	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorElUser(event.currentTarget);
-	};
-
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
-
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
 
 	return (
 		<Box sx={{ display: { xs: "flex", md: "none" } }}>
